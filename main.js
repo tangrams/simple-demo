76a2c0fd3276fa56c2593762280e51ab20312359
Peter Richardson <github@zoomy.net>
2015-05-07 10:55:32 -0400

    simplified js and style

--- main.js
+++ main.js
@@ -1,62 +1,32 @@
-/*jslint browser: true*/
-/*global Tangram, gui */
+/*** Map ***/
 
-(function () {
-    'use strict';
+var map = L.map('map',
+    {'keyboardZoomOffset': .05}
+);
 
-    var locations = {
-        'London': [51.508, -0.105, 15],
-        'New York': [40.70531887544228, -74.00976419448853, 15],
-        'Seattle': [47.609722, -122.333056, 15]
-    };
+var layer = Tangram.leafletLayer({
+    scene: 'scene.yaml',
+    // preUpdate: preUpdate,
+    // postUpdate: postUpdate,
+    attribution: 'Map data &copy; OpenStreetMap'
+});
 
-    var map_start_location = locations['New York'];
+window.layer = layer;
+window.scene = layer.scene;
 
-    /*** URL parsing ***/
+layer.addTo(map);
 
-    // leaflet-style URL hash pattern:
-    // #[zoom],[lat],[lng]
-    var url_hash = window.location.hash.slice(1, window.location.hash.length).split('/');
+map.setView([40.70531887544228, -74.00976419448853], 15);
 
-    if (url_hash.length == 3) {
-        map_start_location = [url_hash[1],url_hash[2], url_hash[0]];
-        // convert from strings
-        map_start_location = map_start_location.map(Number);
-    }
+var hash = new L.Hash(map);
 
-    /*** Map ***/
+// Resize map to window
+function resizeMap() {
+    document.getElementById('map').style.width = window.innerWidth + 'px';
+    document.getElementById('map').style.height = window.innerHeight + 'px';
+    map.invalidateSize(false);
+}
 
-    var map = L.map('map',
-        {'keyboardZoomOffset': .05}
-    );
+window.addEventListener('resize', resizeMap);
+resizeMap();
 
-    var layer = Tangram.leafletLayer({
-        scene: 'scene.yaml',
-        attribution: 'Map data &copy; OpenStreetMap contributors | <a href="https://github.com/tangrams/tangram" target="_blank">Source Code</a>'
-    });
-
-    window.layer = layer;
-    var scene = layer.scene;
-    window.scene = scene;
-
-    map.setView(map_start_location.slice(0, 2), map_start_location[2]);
-
-    var hash = new L.Hash(map);
-    
-    // Resize map to window
-    function resizeMap() {
-        document.getElementById('map').style.width = window.innerWidth + 'px';
-        document.getElementById('map').style.height = window.innerHeight + 'px';
-        map.invalidateSize(false);
-    }
-
-    window.addEventListener('resize', resizeMap);
-    resizeMap();
-
-    window.addEventListener('load', function () {
-        // Scene initialized
-        layer.addTo(map);
-    });
-
-
-}());
