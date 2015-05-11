f78fa27e5ba92c4d6b07f44f6f5d18e7b5e5cda4
Brett Camper <bcamper@alum.mit.edu>
2015-05-11 19:05:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 /*** Map ***/
 
-var map = L.map('map',
-    {'keyboardZoomOffset': .05}
-);
+var map = L.map('map');
 
 var layer = Tangram.leafletLayer({
     scene: 'scene.yaml',
     attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
 });
-
-window.layer = layer;
-window.scene = layer.scene;
 
 layer.addTo(map);
 
