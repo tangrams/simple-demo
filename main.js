c3444e7d0d6462eefdde1b1614f7e69042fa10d5
Peter Richardson <m33tar@gmail.com>
2015-05-15 18:37:18 -0400

    simpler window resize

--- main.js
+++ main.js
@@ -13,12 +13,3 @@ map.setView([40.70531887544228, -74.00976419448853], 15);
 
 var hash = new L.Hash(map);
 
-// Resize map to window
-function resizeMap() {
-    document.getElementById('map').style.width = window.innerWidth + 'px';
-    document.getElementById('map').style.height = window.innerHeight + 'px';
-    map.invalidateSize(false);
-}
-
-window.addEventListener('resize', resizeMap);
-resizeMap();
